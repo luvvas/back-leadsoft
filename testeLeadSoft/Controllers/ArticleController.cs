981270c012dba0d82d7808531efa902e79621b8c
luvvas <lucasmachado1@ufpr.br>
2022-11-24 03:27:58 -0300

    adding article migration

---
+++ testeLeadSoft/Controllers/ArticleController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using testeLeadSoft.Data;
+
+namespace testeLeadSoft.Controllers
+{
+	public class ArticleController : ControllerBase
+	{
+		private readonly DataContext context;
+		public ArticleController(DataContext context) 
+		{
+			this.context = context;
+		}
+	}
+}
